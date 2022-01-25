463ced94e06e507a82126cd15955fd77a34538cb
NicolasDubois187 <nicolasdubois187.pro@gmail.com>
2022-01-25 23:13:54 +0100

    sass-myWork

---
+++ index.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+
+window.addEventListener("scroll", () => {
+console.log(window.scrollY);
+
+  if (window.scrollY > 400) {
+    logoSkills.style.transform = "none";
+  }
+  if (window.scrollY > 1200) {
+      int1.style.opacity = 1;
+      int2.style.opacity = 1;
+      int3.style.opacity = 1;
+      int4.style.opacity = 1;
+  }
+});
+window.addEventListener("load", () => {
+    idPicture.style.opacity = 1;
+});
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
