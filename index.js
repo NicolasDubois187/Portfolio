fcb0a26ea7bec54bc01f21242215c1e3e5ebf5f9
NicolasDubois187 <nicolasdubois187.pro@gmail.com>
2022-02-03 23:36:40 +0100

    portfolio-V1.0

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 
 const form = document.querySelector("form");
-let name1Content;
-let emailContent;
-let messageContent;
+
 
 window.addEventListener("scroll", () => {
   
 console.log(window.scrollY);
 
-  if (window.scrollY > 400) {
+  if (window.scrollY > 350) {
     logoSkills.style.transform = "none";
   }
-  if (window.scrollY > 1200) {
+  else {
+    logoSkills.style.transform = "translateX(250%)";
+  }
+  if (window.scrollY > 1300) {
       int1.style.opacity = 1;
       int2.style.opacity = 1;
       int3.style.opacity = 1;
       int4.style.opacity = 1;
+      
+  }
+  else {
+    int1.style.opacity = 0;
+    int2.style.opacity = 0;
+    int3.style.opacity = 0;
+    int4.style.opacity = 0;
+  }
+  if (window.scrollY > 250) {
+    idPicture.style.opacity = 0;
+  }
+  else {
+    idPicture.style.opacity = 1;
   }
 });
 window.addEventListener("load", () => {
@@ -26,18 +40,24 @@ form.addEventListener("submit", (e) => {
 
   console.log();
 
+  if (!email.value.match(/[a-z0-9_\-\.]+@[a-z0-9_\-\.]+\.[a-z]+/i) )
+   {
+    alert(email.value + " n'est pas une adresse valide");  
+  } 
   if (
-    name1Content.length > 1 &&
-    emailContent.length > 1 &&
-    messageContent.length > 1
-  ) {
-    alert("BINGO");
-    name.value = "";
+    name1.value < 1 |
+    message.value < 1
+  ) 
+  {
+  alert("Merci de bien renseigner les champs");
+  }
+  
+  else {
+    alert("Message envoyé !!!");
+    name1.value = "";
     email.value = "";
     message.value = "";
     
-  } else {
-    alert("Merci de bien renseigner les champs");
   }
 
 });
